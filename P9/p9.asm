66ca467ca12249215d6fd589a075bc4f1ced155e
jorgecastro82-svg <jorge.castro82@uabc.edu.mx>
2025-11-13 04:51:35 -0800

    practica 9 terminada

--- P9/p9.asm
+++ P9/p9.asm
@@ -13,32 +13,24 @@ global _start:
 
 _start:
 
-    ;impresion arreglo 1
-    mov ecx,N 
-    mov ebx,array1
-    call printArrayHex
-    call line_fit
-
     ;captura arreglo (en el 1)
     mov ecx,N 
     mov ebx,array1
-    call inpArray
+    call capturarArreglo
     call line_fit
 
-    ;impresion arreglo 1 con datos capturados
-    mov ecx,N 
-    mov ebx,array1
-    call printArrayHex
+    ;captura arreglo (en el 2)
+    mov ebx,array2
+    call capturarArreglo
     call line_fit
 
-    ;impresion suma de arreglos
-    mov ecx,N 
+    ;impresion producto punto
     mov ebx,array1
     mov edx,array2
-    call vectorSum
+    call productoEscalar
 
-    mov ecx,N 
-    mov ebx,array1
+    ;impresion suma de arreglos
+    call vectorSum
     call printArrayHex
     call line_fit
 
@@ -55,29 +47,40 @@ line_fit:
     ret
 
 printArrayHex:          ;se ocupa que la direccion del vector este en ebx y ecx tener el valor de N
+    push ecx
+    push edi
+    push eax
     cmp ecx,10          
     jg exit             ;ecx depende como maximo tiene que ser 10
     cmp ecx,0
     jl exit             ;caso de que sea menor a 10
     jz exit             ;caso de que sea 0
-print:
-    mov edi,N 
-    sub edi,ecx         
+
+    mov edi,0
+print:        
     mov al,[ebx+edi]
     call pHex_b
     call line_fit
+    inc edi
     loop print
 exit:
+    pop eax
+    pop edi
+    pop ecx
     ret
 
 capturarArreglo:
     ;validacion de ecx
+    push ecx
+    push eax
+    push edi
     cmp ecx,10          
     jg exit2             ;ecx depende como maximo tiene que ser 10
     cmp ecx,0
     jl exit2             ;caso de que sea menor a 10
     jz exit2             ;caso de que sea 0
 
+    mov edi,ebx
 inpArray:
     ;pedir y validacion
     call getch            
@@ -85,21 +88,25 @@ inpArray:
     jl inpArray
     cmp al,'9'
     jg inpArray
-
     ;si paso por aqui esta en el rango de 0-9 ascii
     call putchar        ;impresion del valor
-    call line_fit
     sub al,'0'          ;conversion de ascii a numerico
-    mov edi,N           ;inidice = N-ecx
-    sub edi,ecx         
-    mov [ebx+edi],al
+    mov [edi],al
+    inc edi
+    call line_fit
     loop inpArray
 
     exit2:
+    pop edi
+    pop eax
+    pop ecx
     ret
 
 vectorSum:
     ;validacion de ecx
+    push ecx
+    push edi
+    push eax
     cmp ecx,10          
     jg exit3             ;ecx depende como maximo tiene que ser 10
     cmp ecx,0
@@ -107,14 +114,52 @@ vectorSum:
     jz exit3             ;caso de que sea 0
     ;si pasa por aqui ecx es valido
 
+    mov edi,0
 suma:
-    mov edi,N           
-    sub edi,ecx     ;inidice = N-ecx
     ;ebx es la direccion del primer arreglo y edx la del segundo, resultado se guardara en el primer arreglo
     mov al,[ebx+edi]
     add al,[edx+edi]
     mov [ebx+edi],al
-
+    inc edi
     loop suma
 exit3:
+    pop eax
+    pop edi
+    pop ecx
+    ret
+
+productoEscalar:
+    ;validacion de ecx
+    push ecx
+    push edi
+    push eax
+    push esi
+    cmp ecx,10          
+    jg exit4             ;ecx depende como maximo tiene que ser 10
+    cmp ecx,0
+    jl exit4             ;caso de que sea menor a 10
+    jz exit4             ;caso de que sea 0
+    ;si pasa por aqui ecx es valido
+
+    mov edi,0
+    mov esi,0
+    mov eax,0
+producto:
+    ;ebx es la direccion del primer arreglo y edx la del segundo, resultado se guardara en ax
+    mov al,[ebx+edi]
+    mul byte [edx+edi]
+    add si,ax
+    inc edi
+    loop producto
+
+    mov eax,0
+    mov ax,si
+    call pHex_w
+    call line_fit
+    call line_fit
+exit4:
+    pop esi
+    pop eax
+    pop edi
+    pop ecx
     ret
